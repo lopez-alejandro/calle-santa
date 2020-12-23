38e8f0a79d1bb4d6a8e5ccf3da16e481072990c0
Alejandro Lopez <alejandrolopez@salesforce.com>
2020-12-23 11:51:08 -0800

    trying without window reference

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import PostLink from "../components/post-link"
 import HeroHeader from "../components/heroHeader"
 import netlifyIdentity from 'netlify-identity-widget';
 
-window.netlifyIdentity = netlifyIdentity;
 // You must run this once before trying to interact with the widget
 netlifyIdentity.init();
 
