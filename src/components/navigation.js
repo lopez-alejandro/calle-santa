9c581b6837f3e69b39ceeba09e60d434d63fafe5
Alejandro Lopez <alejandrolopez@salesforce.com>
2020-12-23 11:32:22 -0800

    added button with login open

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,12 +3,37 @@ import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 import netlifyIdentity from 'netlify-identity-widget';
 
+const netlifyAuth = {
+  isAuthenticated: false,
+  user: null,
+  authenticate(callback) {
+    this.isAuthenticated = true;
+    netlifyIdentity.open();
+    netlifyIdentity.on('login', user => {
+      this.user = user;
+      callback(user);
+    });
+  },
+  signout(callback) {
+    this.isAuthenticated = false;
+    netlifyIdentity.logout();
+    netlifyIdentity.on('logout', () => {
+      this.user = null;
+      callback();
+    });
+  }
+};
+
+login = () => {
+  netlifyAuth.authenticate(() => {
+    this.setState({ redirectToReferrer: true });
+  });
+};
 
 export default (props) => (
   <nav className="navigation">
-    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     <div data-netlify-identity-menu></div>
-    <div data-netlify-identity-button>Login</div>
+    <div data-netlify-identity-button class="button" onclick={login}></div>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
