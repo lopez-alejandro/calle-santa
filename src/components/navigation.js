b122ac9307e99c4d87f34696f739bc2198690ac6
Alejandro Lopez <alejandrolopez@salesforce.com>
2020-12-22 23:30:39 -0800

    adding login buttons

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,8 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
+    <div data-netlify-identity-menu></div>
+    <div data-netlify-identity-button>Login</div>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
