3b4f254bf2081e09f76ac39c5282702f1dfaf441
Alejandro Lopez <alejandrolopez@salesforce.com>
2020-12-22 23:39:26 -0800

    identify callbacks

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,6 +2,15 @@ import React from "react"
 import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
 export default (props) => (
   <nav className="navigation"> 
     <div data-netlify-identity-menu></div>
